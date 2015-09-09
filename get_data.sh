5dfb6c3f3fcf8cfc090d806e8cf91709e99f1906
Richard Marko <rmarko@fedoraproject.org>
2015-09-09 17:10:27 +0200

    add get_data.sh script

---
+++ get_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -d data ]; then
+    echo "data directory already present, exiting"
+    exit 1
+fi
+
+mkdir data
+wget --recursive --level=1 --cut-dirs=3 --no-host-directories \
+  --directory-prefix=data --accept '*.gz' http://yann.lecun.com/exdb/mnist/
+pushd data
+gunzip *
+popd
