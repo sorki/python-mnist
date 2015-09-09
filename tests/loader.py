333b1f6ca89cac2240c7496f4bf0c298f6b89a0f
Richard Marko <rmarko@fedoraproject.org>
2015-09-09 16:47:40 +0200

    add tests

---
+++ tests/loader.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import os
+import sys
+import logging
+import unittest
+
+cpath = os.path.dirname(os.path.realpath(__file__))
+root_path = os.path.abspath(os.path.join(cpath, ".."))
+sys.path.insert(0, root_path)
+os.environ["PATH"] = "{0}:{1}".format(root_path, os.environ["PATH"])
+
+DATA_PATH = os.path.join(root_path, 'data')
+
+import mnist
+
+
+class LoaderTestCase(unittest.TestCase):
+    def test_dataset_lengths(self):
+        mn = mnist.MNIST(DATA_PATH)
+
+        test_img, test_label = mn.load_testing()
+        train_img, train_label = mn.load_training()
+        self.assertEqual(len(test_img), len(test_label))
+        self.assertEqual(len(test_img), 10000)
+        self.assertEqual(len(train_img), len(train_label))
+        self.assertEqual(len(train_img), 60000)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    unittest.main()
